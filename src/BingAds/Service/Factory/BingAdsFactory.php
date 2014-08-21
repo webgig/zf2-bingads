2dbda181aafb6ebbc3d8df2b86da21af3aeb62a7
Sagar Bhandari <webgig.sagar@gmail.com>
2014-08-21 23:19:13 +1000

    Remove test codes

--- src/BingAds/Service/Factory/BingAdsFactory.php
+++ src/BingAds/Service/Factory/BingAdsFactory.php
@@ -17,7 +17,6 @@ class BingAdsFactory implements FactoryInterface
         $config          = $serviceLocator->get('Config');
         $bing_api_config = $config['bing-api-config'];
 
-        $bing_api_config['redirect_uri'] = "http://jimmy.com/authcallback";
         $config = array(
                     'user_agent'      => $bing_api_config['user_agent'],
                     'client_id'       => $bing_api_config['client_id'],
@@ -25,7 +24,7 @@ class BingAdsFactory implements FactoryInterface
                     'redirect_uri'    => $bing_api_config['redirect_uri'],
                     'developer_token' => $bing_api_config['developer_token']
          );
-        //var_dump($config);exit;
+
         $bing_api_service = new \BingAds\Service\BingAds($config);
         return $bing_api_service;
     }
